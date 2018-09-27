b22676f7e1949b6acb942e9602bcd5840f50d3fc
Zachary Chaney <zchaney@mines.edu>
2018-09-27 11:47:17 -0600

    Increased head size and moved eyes

--- src/Cat.java
+++ src/Cat.java
@@ -5,10 +5,10 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 100;
-	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
+	private static final int HEAD_DIMENSION = 200;
+	// eyes will be about 1/3 from top of head and 1/3 from left
+	private static final int EYE_Y = HEAD_DIMENSION/3;
+	private static final int EYE_X = HEAD_DIMENSION/3;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 20;
