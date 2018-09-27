7347ea6f68f15ef777cd620e844da1355ec817f9
Zachary Aparicio <zaparicio@mines.edu>
2018-09-27 12:04:38 -0600

    Added second cat and changed text color

--- src/Cat.java
+++ src/Cat.java
@@ -48,7 +48,7 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.black);
+		g2.setColor(Color.red);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("Meowww", catX, catY+HEAD_DIMENSION+10);	
--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -14,15 +14,18 @@ import javax.swing.JPanel;
 
 public class MyDrawing extends JPanel {
 	private Cat cat;
+	private Cat cat2;
 	public MyDrawing() 
 	{
 		// Add critters to the drawing
 		cat = new Cat();
+		cat2 = new Cat();
 	}
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 100, 75);
+		cat2.draw(g,  30, 30);
 	}
 	
 	/**
